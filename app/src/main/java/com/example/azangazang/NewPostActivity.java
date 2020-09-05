d7540feb2b9e765f147e1fe11c1d741fbe20393d
Seungmin Yi <s2019w33@e-mirim.hs.kr>
2020-09-06 00:06:16 +0900

    #12 - New post page UI and Function finish(강의 9번째 finish)

--- app/src/main/java/com/example/azangazang/NewPostActivity.java
+++ app/src/main/java/com/example/azangazang/NewPostActivity.java
@@ -1,19 +1,166 @@
 package com.example.azangazang;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewPostActivity extends AppCompatActivity {
+
+    private ImageView newPostImage;
+    private EditText newPostTitle;
+    private Button newPostBtn;
+
+    private Uri postImageUri = null;
+
+    private ProgressBar newPostProgressBar;
+
+    private StorageReference storageReference;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth firebaseAuth;
+
+    private String current_user_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
 
+        storageReference = FirebaseStorage.getInstance().getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        current_user_id = firebaseAuth.getCurrentUser().getUid();
+
         androidx.appcompat.app.ActionBar ab = getSupportActionBar();
         ab.show();
         ab.setTitle("리뷰 쓰기");
         ab.setDisplayHomeAsUpEnabled(true);
+
+        newPostImage = findViewById(R.id.new_post_image);
+        newPostTitle = findViewById(R.id.post_title);
+        newPostBtn = findViewById(R.id.post_blog);
+        newPostProgressBar = findViewById(R.id.new_post_progress);
+
+        newPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .setMinCropResultSize(700, 512)
+                        .setAspectRatio(1, 1)
+                        .start(NewPostActivity.this);
+            }
+        });
+
+        newPostBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String title = newPostTitle.getText().toString();
+
+                if (!TextUtils.isEmpty(title) && postImageUri != null) {
+
+                    newPostProgressBar.setVisibility(View.VISIBLE);
+
+                    String randomName = FieldValue.serverTimestamp().toString();
+
+                    final StorageReference filePath = storageReference.child("post_images").child(randomName + ".jpg");
+                    UploadTask uploadTask = filePath.putFile(postImageUri);
+
+                    Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                        @Override
+                        public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                            if (!task.isSuccessful()) {
+                                throw task.getException();
+                            }
+
+                            // Continue with the task to get the download URL
+                            return filePath.getDownloadUrl();
+                        }
+                    }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+
+                            if (task.isSuccessful()) {
+
+                                String downloadUri = task.getResult().toString();
+
+                                Map<String, Object> postMap = new HashMap<>();
+                                postMap.put("image_uri", downloadUri);
+                                postMap.put("title", title);
+                                postMap.put("user_id", current_user_id);
+                                postMap.put("timestamp", FieldValue.serverTimestamp());
+
+                                firebaseFirestore.collection("Posts").add(postMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<DocumentReference> task) {
+
+                                        if (task.isSuccessful()) {
+
+                                            Toast.makeText(NewPostActivity.this, "리뷰가 추가되었습니다!", Toast.LENGTH_LONG).show();
+                                            Intent mainIntent = new Intent(NewPostActivity.this, MainActivity.class);
+                                            startActivity(mainIntent);
+
+                                        } else {
+
+
+                                        }
+                                        newPostProgressBar.setVisibility(View.INVISIBLE);
+                                    }
+                                });
+                            } else {
+                                newPostProgressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+                postImageUri = result.getUri();
+                newPostImage.setImageURI(postImageUri);
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
